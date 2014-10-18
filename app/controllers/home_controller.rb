5f3bbf40f2e7dd1f0dab4497576e7b3425d595eb
harrychen <harrychen-cjz@hotmail.com>
2014-10-18 01:49:05 -0700

    add google analytics

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,41 +1,54 @@
 class HomeController < ApplicationController
   def home
+    @title = "主页"
   end
 
   def about
+    @title = "公司简介"
   end
 
   def team
+    @title = "核心团队"
   end
 
   def info
+    @title = "我们的优势"
   end
 
   def contact
+    @title = "联系我们"
   end
 
   def fuwu
+    @title = "广贤学舍-服务范围"
   end
 
   def quxiang
+    @title = "广贤学舍-学生去向"
   end
 
   def anli
+    @title = "广贤学舍-成功案例"
   end
 
   def xuezhi
+    @title = "广贤学舍-2+2学制"
   end
 
   def fuhua
+    @title = "名校孵化器"
   end
 
   def mingxiaofuwu
+    @title = "名校孵化器-服务范围"
   end
 
   def mingxiaoanli
+    @title = "名校孵化器-典型案例"
   end
 
   def mingxiaoyoushi
+    @title = "名校孵化器-我们的优势"
   end
 
   def admin
